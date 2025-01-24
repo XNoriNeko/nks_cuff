aaa5f7abafa7adfd63bf500af933c8ebcc1bbc61
MapleOS <75733106+XNoriNeko@users.noreply.github.com>
2025-01-24 14:38:12 +0100

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -10,8 +10,8 @@ server_script 'server/server.lua'
 shared_script '@es_extended/imports.lua'
 
 files {
-    'stream/p_cs_cuffs_02_s.ydr',  -- Ajout du fichier de stream
-    'config.json'  -- Ajout du fichier config.json
+    'stream/p_cs_cuffs_02_s.ydr',
+    'config.json'
 }
 
-data_file 'DLC_ITYP_REQUEST' 'stream/p_cs_cuffs_02_s.ydr'  -- Ajout de la ligne pour le fichier de stream
+data_file 'DLC_ITYP_REQUEST' 'stream/p_cs_cuffs_02_s.ydr'
